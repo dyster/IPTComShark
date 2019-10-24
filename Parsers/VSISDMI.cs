11887c5a2a5b9e0e4fc919d9ea7d198a045d1a39
Johan Sonesson <johan@traintech.eu>
2019-10-25 00:08:14 +0100

    Add skeleton VSISDMI class

---
+++ Parsers/VSISDMI.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using sonesson_tools.BitStreamParser;
+
+namespace IPTComShark.Parsers
+{
+    public class VSISDMI : DataSetCollection
+    {
+        public VSISDMI()
+        {
+            this.Name = "VSIS DMI";
+            this.Description = "EVC Telegrams";
+        }
+    }
+}
