2751a7dcf303b39ba47e0b7ea368c9b1cfa056cb
Johan Sonesson <johan@traintech.eu>
2020-12-25 00:40:44 +0000

    Change back to x64 only

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace IPTComShark.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.5.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.8.1.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -168,7 +168,7 @@ namespace IPTComShark.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("MMI_M_PACKET\r\nMMI_L_PACKET\r\n        ")]
+        [global::System.Configuration.DefaultSettingValueAttribute("MMI_M_PACKET\r\nMMI_L_PACKET")]
         public string IgnoreVariables {
             get {
                 return ((string)(this["IgnoreVariables"]));
