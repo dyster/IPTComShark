6b6874405b20e5b7669377f70c945a14c429d400
Johan Sonesson <johan@traintech.eu>
2023-12-11 14:45:26 +0000

    Add ORT parsing to BDS packet

--- iptshark/CapturePacket/BDSPacket.cs
+++ iptshark/CapturePacket/BDSPacket.cs
@@ -2,11 +2,16 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace IPTComShark
 {
     internal class BDSPacket : PacketDotNet.Packet, iPacket
     {
+        private static byte[] txtstartbytes = ASCIIEncoding.ASCII.GetBytes("ORT Data ModuleID: ");
+        private static Regex csqRegex = new Regex(@"0d 0a \+CSQ: (\d+),(\d+) 0d 0a 0d( 0a OK 0d 0a)?");
+        private static Regex cregRegex = new Regex(@"0d 0a \+CREG: (\d+),(\d+) 0d 0a 0d( 0a OK 0d 0a)?");
+
         public BDSPacket(byte[] data)
         {
             var headerlength = 27;
@@ -30,8 +35,11 @@ namespace IPTComShark
                     DisplayFields.Add(new DisplayField("txt", ASCIIEncoding.ASCII.GetString(payload)));
                     break;
                 case 0x89:
-                    Name = "BDS_TXT";
-                    DisplayFields.Add(new DisplayField("txt", ASCIIEncoding.ASCII.GetString(payload)));
+                    var name = "BDS_TXT";
+                    
+                    var df = DoText(payload, ref name);
+                    Name = name;
+                    DisplayFields.Add(df);
                     break;
                 default:
                     Name = header[0] + "_???";
@@ -54,6 +62,85 @@ namespace IPTComShark
         public List<DisplayField> DisplayFields { get; set; } = new List<DisplayField>();
 
         public ProtocolType Protocol { get => ProtocolType.BDS; }
+
+        public DisplayField DoText(byte[] payload, ref string name)
+        {
+            //var txt = ASCIIEncoding.ASCII.GetString(payload);
+
+            
+
+            var isMatch = false;
+            for(int i = 0; i < txtstartbytes.Length; i++)
+            {
+                isMatch = txtstartbytes[i] == payload[i];
+                if (!isMatch)
+                    break;
+            }
+
+            if(isMatch)
+            {
+                name = "BDS_ORT";
+                var newlen = payload.Length - txtstartbytes.Length;
+                var txtbytes = new byte[payload.Length - txtstartbytes.Length];
+                Array.Copy(payload, txtstartbytes.Length, txtbytes, 0, newlen);
+
+                var moduleId = ASCIIEncoding.ASCII.GetString(txtbytes, 0, 2);
+                var txt = ASCIIEncoding.ASCII.GetString(txtbytes, 3, txtbytes.Length-3);
+
+                if(csqRegex.IsMatch(txt))
+                {
+                    name = "BDS_ORT_CSQ";
+                    var match = csqRegex.Match(txt);
+
+                    var rssr = Convert.ToUInt16(match.Groups[1].ToString());
+                    /*
+                     * 0 -110dBm or less
+                     * 1 -110dBm to -109dBm
+                     * 2...30 -109... -49dBm
+                     * 31 --48 dBm or greater
+                     * 99 not known or not detectable
+                     */
+                    var ber = Convert.ToUInt16(match.Groups[2].ToString());
+                    /*
+                     * 0...7 as RXQUAL values
+                     * 99 not known or not detectable
+                     */
+
+                    var rettxt = $"RSSR={rssr} BER={ber}";
+                    if (!string.IsNullOrEmpty(match.Groups[3].ToString()))
+                        rettxt += " OK";
+
+                    return new DisplayField("CSQ", rettxt);
+                }
+                else if (cregRegex.IsMatch(txt))
+                {
+                    name = "BDS_ORT_CREG";
+                    var match = cregRegex.Match(txt);
+
+                    var n = Convert.ToUInt16(match.Groups[1].ToString());
+                    
+                    var stat = Convert.ToUInt16(match.Groups[2].ToString());
+                    
+                    var rettxt = $"N={n} STAT={stat}";
+                    if (!string.IsNullOrEmpty(match.Groups[3].ToString()))
+                        rettxt += " OK";
+
+                    return new DisplayField("CREG", rettxt);
+                }
+
+                return new DisplayField(moduleId, txt);
+            }
+            else
+            {
+                return new DisplayField("txt", ASCIIEncoding.ASCII.GetString(payload));
+            }
+
+            var df = new DisplayField("txt", null);
+
+
+
+            return df;
+        }
 
         public string ASCII()
         {
