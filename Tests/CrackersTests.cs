053aee813e46cff04839429f675ff7916a087d37
Johan Sonesson <johan@traintech.eu>
2023-12-11 14:48:39 +0000

    Add test project

---
+++ Tests/CrackersTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BitDataParser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitDataParser.Tests
+{
+    [TestClass()]
+    public class CrackersTests
+    {
+        [TestMethod()]
+        public void CrackUInt16Test()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CrackUInt32Test()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CrackUInt64Test()
+        {
+            Assert.Fail();
+        }
+    }
+}
