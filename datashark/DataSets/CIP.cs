70a2761b73a73aad1ff2f22bab674c30fd88b3ee
Johan Sonesson <johan@traintech.eu>
2024-02-09 13:53:36 +0000

    Update CIP datasets for STM

--- datashark/DataSets/CIP.cs
+++ datashark/DataSets/CIP.cs
@@ -22,12 +22,25 @@ namespace datashark.DataSets
             {
                 new BitField{Name = "PD_MASTER_ATP_GTW", BitFieldType = BitFieldType.UInt8, Length = 8},
                 new BitField{Name = "ATP_GTW_IsAlive", BitFieldType = BitFieldType.UInt8, Length = 8},
-                new BitField{Name = "ATP_INDICATOR_1", BitFieldType = BitFieldType.Bool, Length = 1},
+                new BitField{Name = "ATP_INDICATOR_5", BitFieldType = BitFieldType.Bool, Length = 1},
+                new BitField{Name = "ATP_INDICATOR_6", BitFieldType = BitFieldType.Bool, Length = 1},
                 new BitField{Name = "ATP_BUTTON_1", BitFieldType = BitFieldType.Bool, Length = 1},
                 new BitField{Name = "ATP_BUTTON_2", BitFieldType = BitFieldType.Bool, Length = 1},
                 new BitField{Name = "ATP_BUTTON_3", BitFieldType = BitFieldType.Bool, Length = 1},
                 new BitField{Name = "ATP_BUTTON_4", BitFieldType = BitFieldType.Bool, Length = 1},
                 new BitField{Name = "ATP_BUTTON_5", BitFieldType = BitFieldType.Bool, Length = 1},
+                new BitField{Name = "Spare1", BitFieldType = BitFieldType.UInt8, Length = 3},
+                new BitField{Name = "ATP_FLASHING_INDICATOR_1", BitFieldType = BitFieldType.UInt8, Length = 2},
+                new BitField{Name = "ATP_FLASHING_INDICATOR_2", BitFieldType = BitFieldType.UInt8, Length = 2},
+                new BitField{Name = "ATP_FLASHING_INDICATOR_3", BitFieldType = BitFieldType.UInt8, Length = 2},
+                new BitField{Name = "ATP_FLASHING_INDICATOR_4", BitFieldType = BitFieldType.UInt8, Length = 2},
+                new BitField{Name = "Spare2", BitFieldType = BitFieldType.UInt8, Length = 6},
+                new BitField{Name = "ATP_TEXT_MESSAGE_ACK_ID", BitFieldType = BitFieldType.Int8, Length = 8},
+                new BitField{Name = "ATP_TEXT_MESSAGE_ID_1", BitFieldType = BitFieldType.Int8, Length = 8},
+                new BitField{Name = "ATP_TEXT_MESSAGE_ID_2", BitFieldType = BitFieldType.Int8, Length = 8},
+                new BitField{Name = "ATP_TEXT_MESSAGE_ID_3", BitFieldType = BitFieldType.Int8, Length = 8},
+                new BitField{Name = "ATP_TEXT_MESSAGE_ID_4", BitFieldType = BitFieldType.Int8, Length = 8},
+                new BitField{Name = "Spare3", BitFieldType = BitFieldType.Spare, Length = 8+8+32+32},
             }
         };
 
