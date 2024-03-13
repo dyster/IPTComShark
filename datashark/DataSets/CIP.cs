24f85def9bead1432b8553b6d48b96c4819add79
Johan Sonesson <johan@traintech.eu>
2024-03-13 11:53:41 +0000

    Add more CIP datasets

--- datashark/DataSets/CIP.cs
+++ datashark/DataSets/CIP.cs
@@ -82,5 +82,89 @@ namespace datashark.DataSets
                 
             }
         };
+
+        public static DataSetDefinition TPWStoGW = new DataSetDefinition
+        {
+            Name = "TPWS to GW",
+            BitFields = new List<BitField>
+            {
+                new BitField{Name = "AWS_REC_FAULT", BitFieldType = BitFieldType.Bool, Length = 1},
+                new BitField{Name = "AWS_SELECT_FAULT", BitFieldType = BitFieldType.Bool, Length = 1},
+                new BitField{Name = "AWS_RESET_FAULT", BitFieldType = BitFieldType.Bool, Length = 1},
+                new BitField{Name = "AWS_RESET_POWER_FLT", BitFieldType = BitFieldType.Bool, Length = 1},
+                new BitField{Name = "AWS_CAUTION_ACK_FLT", BitFieldType = BitFieldType.Bool, Length = 1},
+                new BitField{Name = "AWS_SELFTEST_FAIL", BitFieldType = BitFieldType.Bool, Length = 1},
+                new BitField{Name = "Spare1", BitFieldType = BitFieldType.Spare, Length = 1},
+                new BitField{Name = "Spare2", BitFieldType = BitFieldType.Spare, Length = 1},
+                new BitField{Name = "TPWS_ANT_FAULT", BitFieldType = BitFieldType.Bool, Length = 1},
+                new BitField{Name = "TPWS_TONETEST_FAIL", BitFieldType = BitFieldType.Bool, Length = 1},
+                new BitField{Name = "TPWS_SELFTEST_FAIL", BitFieldType = BitFieldType.Bool, Length = 1},
+                new BitField{Name = "TEMP_ISO_SW_FAULT", BitFieldType = BitFieldType.Bool, Length = 1},
+                new BitField{Name = "Spare3", BitFieldType = BitFieldType.Spare, Length = 1},
+                new BitField{Name = "Spare4", BitFieldType = BitFieldType.Spare, Length = 1},
+                new BitField{Name = "Spare5", BitFieldType = BitFieldType.Spare, Length = 1},
+                new BitField{Name = "Spare6", BitFieldType = BitFieldType.Spare, Length = 1},
+                new BitField{Name = "BRAKE_MON_FAULT", BitFieldType = BitFieldType.Bool, Length = 1},
+                new BitField{Name = "12V_SUPPLY_FAULT", BitFieldType = BitFieldType.Bool, Length = 1},
+                new BitField{Name = "3_3V_SUPPLY_FAULT", BitFieldType = BitFieldType.Bool, Length = 1},
+                new BitField{Name = "INTERNAL_SW_FAILED", BitFieldType = BitFieldType.Bool, Length = 1},
+                new BitField{Name = "CLOCK_FAULT", BitFieldType = BitFieldType.Bool, Length = 1},
+                new BitField{Name = "INTERNAL_COMMS_FLT", BitFieldType = BitFieldType.Bool, Length = 1},
+                new BitField{Name = "Spare7", BitFieldType = BitFieldType.Spare, Length = 1},
+                new BitField{Name = "Spare8", BitFieldType = BitFieldType.Spare, Length = 1},
+                new BitField{Name = "DMI_FAILED", BitFieldType = BitFieldType.Bool, Length = 1},
+                new BitField{Name = "ETCS_COMMANDED_FA", BitFieldType = BitFieldType.Bool, Length = 1},
+                new BitField{Name = "AWS_TPWS_NOT_OP", BitFieldType = BitFieldType.Bool, Length = 1},
+                new BitField{Name = "INVALID_AWS_TPWS_OP", BitFieldType = BitFieldType.Bool, Length = 1},
+                new BitField{Name = "MISSING_FUNCTION", BitFieldType = BitFieldType.Bool, Length = 1},
+                new BitField{Name = "CAB_STATUS_INVALID", BitFieldType = BitFieldType.Bool, Length = 1},
+                new BitField{Name = "INVALID_STATE_TRANS", BitFieldType = BitFieldType.Bool, Length = 1},
+                new BitField{Name = "VERSION_ERROR", BitFieldType = BitFieldType.Bool, Length = 1},
+                new BitField{Name = "NO_SELFTEST_PASSED", BitFieldType = BitFieldType.Bool, Length = 1},
+                new BitField{Name = "SAP_DISCONNECT_BIU", BitFieldType = BitFieldType.Bool, Length = 1},
+                new BitField{Name = "SAP_DISCONNECT_DMI", BitFieldType = BitFieldType.Bool, Length = 1},
+                new BitField{Name = "Speech Unit FLT", BitFieldType = BitFieldType.Bool, Length = 1},
+                new BitField{Name = "Spare9", BitFieldType = BitFieldType.Spare, Length = 1},
+                new BitField{Name = "Spare10", BitFieldType = BitFieldType.Spare, Length = 1},
+                new BitField{Name = "Spare11", BitFieldType = BitFieldType.Spare, Length = 1},
+                new BitField{Name = "Spare12", BitFieldType = BitFieldType.Spare, Length = 1},
+                new BitField{Name = "SPAD_BRAKE_MESS", BitFieldType = BitFieldType.Bool, Length = 1},
+                new BitField{Name = "OSS_BRAKE_MESS", BitFieldType = BitFieldType.Bool, Length = 1},
+                new BitField{Name = "AWS_BRAKE_MESS", BitFieldType = BitFieldType.Bool, Length = 1},
+                new BitField{Name = "TEMP_ISO_MESS", BitFieldType = BitFieldType.Bool, Length = 1},
+                new BitField{Name = "TPWS_FAULT MESS", BitFieldType = BitFieldType.Bool, Length = 1},
+                new BitField{Name = "AWS_ISOLATION MESS", BitFieldType = BitFieldType.Bool, Length = 1},
+                new BitField{Name = "AWS_FAULT MESS", BitFieldType = BitFieldType.Bool, Length = 1},
+                new BitField{Name = "Confirm activation of TSO button MESS", BitFieldType = BitFieldType.Bool, Length = 1},
+                new BitField{Name = "Brake Release MESS", BitFieldType = BitFieldType.Bool, Length = 1},
+                new BitField{Name = "Train Stop Overide Active MESS", BitFieldType = BitFieldType.Bool, Length = 1},
+
+            }
+        };
+
+        public static DataSetDefinition GWtoEVC = new DataSetDefinition
+        {
+            Name = "GW to EVC",
+            BitFields = new List<BitField>
+            {
+                new BitField{Name = "TCMS_LifeSign", BitFieldType = BitFieldType.UInt8, Length = 8},
+                new BitField{Name = "Service_Brake_active_Vehicle", BitFieldType = BitFieldType.Bool, Length = 1},
+                new BitField{Name = "TR_OBU_Traction_Status", BitFieldType = BitFieldType.Bool, Length = 1},
+                new BitField{Name = "SpareC1", BitFieldType = BitFieldType.Spare, Length = 1},
+                new BitField{Name = "SpareC2", BitFieldType = BitFieldType.Spare, Length = 1},
+                new BitField{Name = "SpareC3", BitFieldType = BitFieldType.Spare, Length = 1},
+                new BitField{Name = "SpareC4", BitFieldType = BitFieldType.Spare, Length = 1},
+                new BitField{Name = "SpareC5", BitFieldType = BitFieldType.Spare, Length = 1},
+                new BitField{Name = "SET_SPEED_DISPLAY_INFO", BitFieldType = BitFieldType.Bool, Length = 1},
+                new BitField{Name = "SpareC6", BitFieldType = BitFieldType.Spare, Length = 8},
+                new BitField{Name = "TR_OBU_TrainType", BitFieldType = BitFieldType.UInt8, Length = 8},
+                new BitField{Name = "SET_TARGET_SPEED", BitFieldType = BitFieldType.Int32, Length = 32},
+                new BitField{Name = "Driver_id_begin", BitFieldType = BitFieldType.UInt64, Length = 64},
+                new BitField{Name = "Driver_id_end", BitFieldType = BitFieldType.UInt64, Length = 64},
+                new BitField{Name = "SpareC7", BitFieldType = BitFieldType.Spare, Length = 32},
+                new BitField{Name = "SpareC8", BitFieldType = BitFieldType.Spare, Length = 32},
+
+            }
+        };
     }
 }
